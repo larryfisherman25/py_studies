5194601000b4431a3192751dfd55ec5a506c5bcc
larryfisherman25 <matheusfgiraldes@gmail.com>
2022-08-09 18:54:52 -0300

    09-08-22 studies

--- automation_cookbook/recipe_format_strings_step1.py
+++ automation_cookbook/recipe_format_strings_step1.py
@@ -1,5 +1,5 @@
 data = [
-    (1000, 10),
+    (123456, 10),
     (2000, 17),
     (2500, 170),
     (2500, -170),
@@ -15,4 +15,3 @@ TEMPLATE = '{revenue:>7,} | {profit:>+6} | {percent:>7.2%}'
 for revenue, profit in data:
     row = TEMPLATE.format(revenue=revenue, profit=profit, percent=profit/revenue)
     print(row)
-
